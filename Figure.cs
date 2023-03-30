79e21ccbb7c7a4942f322880e2677a3000bfc76e
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2023-03-30 19:00:10 +0300

    Решение на C#

---
+++ Figure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Canculate_Figur
+{
+    interface Figure
+    {
+        double GetArea(); // Метод для вычисления площади фигуры.
+        double GetPerimeter();// Метод для вычисления периметра фигуры.
+        string GetName();// Метод для получения названия фигуры.
+    }
+}
---
+++ Treangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Canculate_Figur
+{
+    //Треугольник
+    class Treangle:Figure
+    {
+        private double sideA;
+        private double sideB;
+        private double sideC;
+
+        public Treangle(double sideA, double sideB, double sideC)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+        }
+
+        //Вычисления площади 
+        public double GetArea()
+        {
+            double p = (sideA + sideB + sideC) / 2;
+            return Math.Sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+        }
+        //Вычисления периметра 
+        public double GetPerimeter()
+        {
+            return sideA + sideB + sideC;
+        }
+
+        //Получения имени фигуры
+        public string GetName()
+        {
+            return "Треугольник";
+        }
+    }
+}
